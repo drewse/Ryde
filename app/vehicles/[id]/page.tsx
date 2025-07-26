287cf901dedb91a18ec25e3baa92652a7877d262
Andrew Scott <andrewscottgm@gmail.com>
2025-07-26 18:14:15 -0400

    Fix Vercel build: remove PageProps and use inline params type

--- app/vehicles/[id]/page.tsx
+++ app/vehicles/[id]/page.tsx
@@ -1,17 +1,15 @@
 // app/vehicles/[id]/page.tsx
-import { Metadata } from 'next';
 import VehicleDetail from './VehicleDetail';
 
 export async function generateStaticParams() {
   return ['1', '2', '3', '4', '5'].map((id) => ({ id }));
 }
 
-type Params = {
-  params: {
-    id: string;
-  };
-};
-
-export default function VehicleDetailPage({ params }: Params) {
-  return <VehicleDetail vehicleId={params.id} />;
+export default function VehicleDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
+  return <VehicleDetail vehicleId={id} />;
 }
