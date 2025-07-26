22090a2f6f369459032f37d29ed54cb366927044
Andrew Scott <andrewscottgm@gmail.com>
2025-07-26 18:07:23 -0400

    Fix PageProps typing issue for Next.js App Router

--- app/vehicles/[id]/page.tsx
+++ app/vehicles/[id]/page.tsx
@@ -1,14 +1,14 @@
 // app/vehicles/[id]/page.tsx
 import VehicleDetail from './VehicleDetail';
 
-export const generateStaticParams = () => {
-  return ['1', '2', '3', '4', '5'].map((id) => ({ id }));
+type PageProps = {
+  params: { id: string };
 };
 
-export default function VehicleDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export function generateStaticParams() {
+  return ['1', '2', '3', '4', '5'].map((id) => ({ id }));
+}
+
+export default function VehicleDetailPage({ params }: PageProps) {
   return <VehicleDetail vehicleId={params.id} />;
 }
