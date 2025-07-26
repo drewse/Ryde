27d774a9d66ad02680c02b83af3e5a94c539cb13
Andrew Scott <andrewscottgm@gmail.com>
2025-07-26 18:21:19 -0400

    Successful local build with Node 22

--- app/vehicles/[id]/page.tsx
+++ app/vehicles/[id]/page.tsx
@@ -1,15 +1,12 @@
 // app/vehicles/[id]/page.tsx
+
 import VehicleDetail from './VehicleDetail';
 
 export async function generateStaticParams() {
   return ['1', '2', '3', '4', '5'].map((id) => ({ id }));
 }
 
-export default function VehicleDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id } = params;
+export default async function VehicleDetailPage({ params }: { params: { id: string } }) {
+  const id = params.id;
   return <VehicleDetail vehicleId={id} />;
 }
