cfd9f0b25215e938535190397229557b6185eafe
Andrew Scott <andrewscottgm@gmail.com>
2025-07-27 00:21:09 -0400

    page.tsx fix

--- app/vehicles/[id]/page.tsx
+++ app/vehicles/[id]/page.tsx
@@ -1,19 +1,15 @@
 import VehicleDetail from './VehicleDetail';
 
-export default function VehiclePage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  return <VehicleDetail vehicleId={params.id} />;
-}
-
 export async function generateStaticParams() {
   return [
     { id: '1' },
     { id: '2' },
     { id: '3' },
     { id: '4' },
-    { id: '5' },
+    { id: '5' }
   ];
+}
+
+export default function VehiclePage({ params }: { params: { id: string } }) {
+  return <VehicleDetail vehicleId={params.id} />;
 }
