82bb542c5ee55e0bf6059beaa96aad67f915c311
Andrew Scott <andrewscottgm@gmail.com>
2025-07-28 21:03:55 -0400

    Fix VehiclePage type error for Vercel build

--- app/vehicles/[id]/page.tsx
+++ app/vehicles/[id]/page.tsx
@@ -1,18 +1,4 @@
 import VehicleDetail from './VehicleDetail';
-import { FC } from 'react';
-import { type Metadata, type ResolvingMetadata } from 'next';
-
-type PageProps = {
-  params: {
-    id: string;
-  };
-};
-
-const VehiclePage: FC<PageProps> = ({ params }) => {
-  return <VehicleDetail vehicleId={params.id} />;
-};
-
-export default VehiclePage;
 
 export async function generateStaticParams() {
   return [
@@ -22,4 +8,8 @@ export async function generateStaticParams() {
     { id: '4' },
     { id: '5' },
   ];
+}
+
+export default function VehiclePage({ params }: { params: { id: string } }) {
+  return <VehicleDetail vehicleId={params.id} />;
 }
